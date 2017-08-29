49e6e88313e2847f85e20dd78dfb0dcf37bd6033
Sergio Gomes <sergiofgomes117@gmail.com>
2017-08-29 14:15:14 -0600

    implemented custom scopes for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
+
+puts "3 Topics created"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis odio id massa commodo elementum. Donec suscipit metus et ante accumsan, id vehicula nisi elementum. Suspendisse et vestibulum nisi. Vestibulum tincidunt tellus ante, et sagittis ex tristique ac. Nam finibus ex nec nulla vulputate commodo. Suspendisse ipsum turpis, semper et molestie at, facilisis aliquet augue. Sed scelerisque gravida est id vehicula. Fusce eget nibh laoreet, viverra lectus sed, tristique eros. Cras tristique id augue in lobortis. Phasellus dictum erat id nibh tempus, eu venenatis diam gravida. Maecenas vel risus sit amet libero aliquet cursus. Nullam ut eleifend mi, molestie mollis dui. Mauris sed venenatis eros. Aenean vel metus a magna suscipit facilisis. Suspendisse a mauris euismod, finibus libero vitae, sollicitudin erat."
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis odio id massa commodo elementum. Donec suscipit metus et ante accumsan, id vehicula nisi elementum. Suspendisse et vestibulum nisi. Vestibulum tincidunt tellus ante, et sagittis ex tristique ac. Nam finibus ex nec nulla vulputate commodo. Suspendisse ipsum turpis, semper et molestie at, facilisis aliquet augue. Sed scelerisque gravida est id vehicula. Fusce eget nibh laoreet, viverra lectus sed, tristique eros. Cras tristique id augue in lobortis. Phasellus dictum erat id nibh tempus, eu venenatis diam gravida. Maecenas vel risus sit amet libero aliquet cursus. Nullam ut eleifend mi, molestie mollis dui. Mauris sed venenatis eros. Aenean vel metus a magna suscipit facilisis. Suspendisse a mauris euismod, finibus libero vitae, sollicitudin erat.",
+    topic_id: Topic.last.id
   )
 end
 
@@ -16,10 +25,10 @@ puts "10 blog posts created"
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio Title: #{portfolio_item}",
-    subtitle: "My great service",
+    subtitle: "Ruby on Rails",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis odio id massa commodo elementum. Donec suscipit metus et ante accumsan, id vehicula nisi elementum. Suspendisse et vestibulum nisi. Vestibulum tincidunt tellus ante, et sagittis ex tristique ac. Nam finibus ex nec nulla vulputate commodo. Suspendisse ipsum turpis, semper et molestie at, facilisis aliquet augue. Sed scelerisque gravida est id vehicula. Fusce eget nibh laoreet, viverra lectus sed, tristique eros. Cras tristique id augue in lobortis. Phasellus dictum erat id nibh tempus, eu venenatis diam gravida. Maecenas vel risus sit amet libero aliquet cursus. Nullam ut eleifend mi, molestie mollis dui. Mauris sed venenatis eros. Aenean vel metus a magna suscipit facilisis. Suspendisse a mauris euismod, finibus libero vitae, sollicitudin erat.",
     main_image: "http://placehold.it/600x400",
     thumb_image: "http://placehold.it/350x200",
@@ -27,3 +36,15 @@ puts "5 skills created"
 end
 
 puts "9 portfolio items created"
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio Title: #{portfolio_item}",
+    subtitle: "Angular",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis odio id massa commodo elementum. Donec suscipit metus et ante accumsan, id vehicula nisi elementum. Suspendisse et vestibulum nisi. Vestibulum tincidunt tellus ante, et sagittis ex tristique ac. Nam finibus ex nec nulla vulputate commodo. Suspendisse ipsum turpis, semper et molestie at, facilisis aliquet augue. Sed scelerisque gravida est id vehicula. Fusce eget nibh laoreet, viverra lectus sed, tristique eros. Cras tristique id augue in lobortis. Phasellus dictum erat id nibh tempus, eu venenatis diam gravida. Maecenas vel risus sit amet libero aliquet cursus. Nullam ut eleifend mi, molestie mollis dui. Mauris sed venenatis eros. Aenean vel metus a magna suscipit facilisis. Suspendisse a mauris euismod, finibus libero vitae, sollicitudin erat.",
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200",
+  )
+end
+
+puts "1 portfolio items created"
