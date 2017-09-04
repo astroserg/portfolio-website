7c4fda6385406d7475d513be74c09d04beab554a
Sergio Gomes <sergiofgomes117@gmail.com>
2017-09-04 14:28:28 -0600

    implemented view helper for authentication

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def login_helper
+     if current_user.is_a?(User)
+     link_to "Sign Out", destroy_user_session_path, method: :delete
+     else
+     link_to "Sign Up", new_user_registration_path
+     link_to "Sign In", new_user_session_path
+    end
+  end
 end
