01ded3a6a067a157d289ad8f71bcf66d59edc1b7
Sergio Gomes <sergiofgomes117@gmail.com>
2017-09-06 12:22:04 -0600

    implemented authorization engine for portfolio and blog items

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -5,8 +5,13 @@ module CurrentUserConcern
     super || guest_user
   end
 
-  def current_user
-    super || OpenStruct.new(name: "Guest User", first_name: "Guest", last_name: "User", email: "test@test.com")
+  def guest_user
+    guest = GuestUser.new
+    guest.name = "Guest User"
+    guest.first_name = "Guest"
+    guest.last_name = "User"
+    guest.email = "guest@example.com"
+    guest
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 module ApplicationHelper
   def login_helper
-     if current_user.is_a?(User)
-     link_to "Sign Out", destroy_user_session_path, method: :delete
+     if current_user.is_a?(GuestUser)
+      (link_to "Sign Up", new_user_registration_path)
+     "<br>".html_safe +
+     (link_to "Sign In", new_user_session_path)
      else
-     link_to "Sign Up", new_user_registration_path
-     link_to "Sign In", new_user_session_path
+      link_to "Sign Out", destroy_user_session_path, method: :delete
    end
   end
 
