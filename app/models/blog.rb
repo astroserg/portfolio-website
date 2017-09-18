45e57f082a890caf2927821738d965cfd437f045
Sergio Gomes <sergiofgomes117@gmail.com>
2017-09-18 10:59:36 -0600

    implemented custom scope for recent blogs and hide draft posts from non-admins

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -8,4 +8,13 @@ extend FriendlyId
  belongs_to :topic
 
  has_many :comments, dependent: :destroy
+
+
+  def self.featured_blogs
+    limit(2)
+  end
+
+  def self.recent
+    order("created_at DESC")
+  end
 end
