610f1e109939c31904be2f6683c1d9ffd94e595a
Sergio Gomes <sergiofgomes117@gmail.com>
2017-09-19 14:14:49 -0600

    more bug fixes for actioncable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,5 +90,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_cable.allowed_request_origins = ['https://sg-designportfolio.herokuapp.com/', 'http://sg-designportfolio.herokuapp.com/']
- config.action_cable.url = "wss://sg-designportfolio.herokuapp.com/"
+  config.action_cable.url = "wss://sg-designportfolio.herokuapp.com/"
 end
