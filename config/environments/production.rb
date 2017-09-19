edf352e7d97edf3b8f21cd7bd43887b420c814f7
Sergio Gomes <sergiofgomes117@gmail.com>
2017-09-19 13:50:45 -0600

    integrated production calls for actioncable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://sg-designportfolio.herokuapp.com/', 'http://sg-designportfolio.herokuapp.com/']
+ config.action_cable.url = "wss://sg-designportfolio.herokuapp.com/"
 end
